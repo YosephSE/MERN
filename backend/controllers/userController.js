32b37fb47122d0aed777481d99c31d7aece148f9
YosephSE <88291669+YosephSE@users.noreply.github.com>
2024-08-02 00:16:36 +0300

    error handlling middleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,7 @@
-const authUser = (req, res) => {
+import asyncHandler from "express-async-handler";
+
+const authUser = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Auth User" });
-};
+});
 
 export { authUser };
--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { notFound, errorHandler } from "./middlware/errorMiddleware.js";
 import userRoutes from "./routes/userRoute.js";
 const port = process.env.PORT;
 const app = express();
@@ -7,4 +8,8 @@ app.use("/api/users", userRoutes);
 app.get("/", (req, res) => {
   res.json({ message: "Welcome Home" });
 });
+
+app.use(notFound);
+app.use(errorHandler);
+
 app.listen(port, () => console.log(`Server running of port ${port}`));
