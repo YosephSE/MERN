bd983bd7de90e44a3e3e8b7270ef82489485e89e
YosephSE <88291669+YosephSE@users.noreply.github.com>
2024-08-06 02:05:18 +0300

    update profile done

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -36,18 +36,44 @@ const registerUser = asyncHandler(async (req, res) => {
 });
 
 const logoutUser = asyncHandler(async (req, res) => {
-  res.cookie('jwt', '', {
+  res.cookie("jwt", "", {
     httpOnly: true,
-    expires: new Date(0)
-  })
+    expires: new Date(0),
+  });
   res.status(200).json({ message: "User LoggedOut Successfully" });
 });
 
 const getUserProfile = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "get User Profile" });
+  const user = {
+    _id: req.user._id,
+    name: req.user.name,
+    email: req.user.email,
+  };
+  res.status(200).json(user);
 });
 
 const updateUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+    const updatedUser = await user.save();
+    res
+      .status(200)
+      .json({
+        _id: updatedUser._id,
+        name: updatedUser.name,
+        email: updatedUser.email,
+      });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
   res.status(200).json({ message: "update User Profile" });
 });
 
