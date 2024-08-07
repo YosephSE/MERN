96748b1a38a9beaae4ef3892d2463845001b48cd
YosephSE <88291669+YosephSE@users.noreply.github.com>
2024-08-08 00:37:29 +0300

    posts schema added, retrive and creaet post added

---
+++ backend/models/postModel.js
@@ -0,0 +1,56 @@
+import mongoose from "mongoose";
+
+const postSchema = mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+  },
+  authorId: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+  comments: [
+    {
+      commentId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        auto: true,
+      },
+      authorId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+      },
+      content: {
+        type: String,
+        required: true,
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+      },
+    },
+  ],
+});
+
+const Post = mongoose.model("Post", postSchema);
+
+export default Post;
--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,13 +1,48 @@
 import { protect } from "../middlware/authmiddleware.js";
 import express from "express";
 const Router = express.Router();
+import Post from "../models/postModel.js";
+import mongoose from "mongoose";
 
-Router.route("/")
-  .get(protect, (req, res) => {
-    res.json({ message: "Success" });
-  })
-  .post(protect, (req, res) => {
-    res.json({ message: "Success post" });
-  });
+Router.post("/", protect,  async (req, res) => {
+  try {
+    const { title, content, authorId, category, image } = req.body;
+
+    if (!title || !content || !authorId || !category || !image) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    if (!mongoose.Types.ObjectId.isValid(authorId)) {
+      return res.status(400).json({ message: "Invalid authorId format" });
+    }
+
+    const newPost = new Post({
+      title,
+      content,
+      authorId,
+      category,
+      image,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    });
+
+    const savedPost = await newPost.save();
+
+    res.status(201).json(savedPost);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+Router.get("/", async (req, res) => {
+  try {
+    const posts = await Post.find();
+    res.json(posts);
+  } catch (err) {
+    console.error("Error retrieving posts:", err);
+    res.status(500).json({ error: "An error occurred while retrieving posts" });
+  }
+});
 
 export default Router;
